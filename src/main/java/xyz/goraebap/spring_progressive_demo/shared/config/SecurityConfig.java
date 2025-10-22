9487ac995597273f24fd2ad8e7ac16d0fc8ab85b
dev-goraebap <dev.goraebap@gmail.com>
2025-10-22 19:13:09 +0900

    update: 람다 메서드를 참조로 바꿈
    .csrf(csrf -> csrf.disable())
    >>
    .csrf(AbstractHttpConfigurer::disable)

--- src/main/java/xyz/goraebap/spring_progressive_demo/shared/config/SecurityConfig.java
+++ src/main/java/xyz/goraebap/spring_progressive_demo/shared/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,7 +38,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
 
                 // CSRF disabled for API (enable if using session-based auth)
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 // Stateless session (for REST API)
                 .sessionManagement(session ->
