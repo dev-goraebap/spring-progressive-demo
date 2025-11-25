029ee2dd6c3888db8d6dbaad9946ab0eb6265496
dev-goraebap <dev.goraebap@gmail.com>
2025-11-25 22:57:10 +0900

    refactor: htmx boost, view transition
    note: api 사용하여 페이지 전환 시 SPA의 부드러운 느낌 주기

--- src/main/java/xyz/goraebap/spring_progressive_demo/app/main/controller/MainPagesController.java
+++ src/main/java/xyz/goraebap/spring_progressive_demo/app/main/controller/MainPagesController.java
@@ -1,15 +1,21 @@
-package xyz.goraebap.spring_progressive_demo.app.home.controller;
+package xyz.goraebap.spring_progressive_demo.app.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomePageController {
+public class MainPagesController {
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "Spring Progressive Demo");
         return "pages/index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Spring Progressive Demo");
+        return "pages/about";
     }
 }
