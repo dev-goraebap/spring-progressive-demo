cbd3173d58438a3ad5494ba6602fe07b2447ea07
dev-goraebap <dev.goraebap@gmail.com>
2025-11-30 14:10:20 +0900

    feat: 피드 목록에 조회수, 댓글수, 태그, 발행일 표시

--- src/main/java/xyz/goraebap/spring_progressive_demo/infra/view_model/PostViewModel.java
+++ src/main/java/xyz/goraebap/spring_progressive_demo/infra/view_model/PostViewModel.java
@@ -3,6 +3,7 @@ package xyz.goraebap.spring_progressive_demo.infra.view_model;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class PostViewModel {
@@ -14,6 +15,7 @@ public class PostViewModel {
     private String thumbnailKey;
     private String thumbnailMetadata;
     private LocalDateTime publishedAt;
+    private List<String> tags;
 
     // After Binding
     private String thumbnailUrl;
